8adf7965bd99a5d2d0da423c63ad0497dcd707f9
NastyaSaprick <sapronovaas2002@gmail.com>
2022-01-17 12:21:39 +0300

    класы и наследование

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
+//1.
+// class First {
+//   constructor(hello){
+//     this.hello = hello;
+//   }
+// }
+// const first = new First('Привет я метод родителя!')
+// console.log(first)
+
+//2.
+// const First = function (hello){
+//   this.hello = hello
+// }
+// First.prototype.sayHello = function () {
+//   console.log(`Здарова ${this.hello}`);
+// }
+
+// const first = new First('Привет я метод родителя!')
+// first.sayHello()
+// console.log(first);
+
+
+//3.
 class First {
-  constructor(hello ){
-    this.hello = hello
+constructor(hello){
+    this.hello = hello;
+  }
+  sayHello = function () {
+    console.log(`Здарова ${this.hello}`);
   }
 }
 
-const first = new First('Привет я метод родителя!')
+//наследование
+class Second extends First{
+  constructor(hello, hello1 = ['А я наследуемый метод!']) {
+    super(hello)
+    this.hello1 = hello1
+  }
+}
 
-//Привет я метод родителя!
+const second = new Second('Привет я метод родителя!')
+console.log(second);
+second.sayHello();
+//А я наследуемый метод!
